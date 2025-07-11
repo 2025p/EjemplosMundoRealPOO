8d5ab9ef99d7e0be9133a7862d32584e660560ee
2025p <wr.cernat@uea.edu.ec>
2025-07-11 00:36:35 -0500

    Proyecto7

---
+++ src/main/java/Clase7.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Clase7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Random rand=new Random();
+        rand.nextlnt();
+        */
+        Motocicleta m1=new Motocicleta("Kawasaki", "H2R");
+        m1.acelerar();
+        
+         Motocicleta m2=new Motocicleta("SKU Ninja");
+         m1.acelerar(200);
+    }
+    
+}
