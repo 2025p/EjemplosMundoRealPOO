3f92dcfdf2835f736bbd3975557ace871030ec3c
2025p <wr.cernat@uea.edu.ec>
2025-10-27 22:11:35 -0500

    Clase16

--- src/main/java/ConversorDeMedidas.java
+++ src/main/java/ConversorDeMedidas.java
@@ -12,46 +12,55 @@ import java.util.Scanner;
  */
 public class ConversorDeMedidas {
     public static void main(String[]args){
-        // Este programa nos va a converciones de pulgadas a milimetros, de yardas a metros y millas a kilometros.
+        // Este programa nos va a converciones de pulgadas a centimetros, de yardas a metros y millas a kilometros.
         // las opciones:
-        // 1 pulgada equivale a 25.40 milimetros.
+        // 1 pulgada equivale a 2.54 milimetros.
         // 1 yarda equivale a 0.91 metros.
         // 1 kilometro equivale a 0.62 millas.
+        // 1 kilogramo equivale a 2.2 libra.
    
          Scanner lector = new Scanner(System.in);
     
          int opc;
-         double pulgadas, milimetros, yardas, metros, kilometros, millas;
+         double pulgadas, centimetros, yardas, metros, kilometros, millas, kilogramos, libras;
       
          System.out.println("Ingresse la opcion que desea:");
-         System.out.println("1. Pulgadas a milimetros.");
+         System.out.println("1. Pulgadas a centimetros.");
          System.out.println("2. Yardas a metros.");
          System.out.println("3. kilometros a Millas.");
+         System.out.println("4. kilogramos a Libras.");
     
          opc = lector.nextInt();
          switch  (opc){
              case 1: 
                  System.out.println("Ingrese  la cantidad de pulgadas a convertir:");
                  pulgadas = lector. nextDouble();
-                 milimetros = pulgadas * 25.40;
-                 System.out.println(pulgadas+"pulgadas equivalen a "+milimetros+"milimetros");
+                 centimetros = pulgadas * 2.54;
+                 System.out.println(pulgadas+"pulgadas equivalen a "+centimetros+"Centimetros");
                  break;
                  
              case 2:
                  System.out.println("Ingrese  la cantidad de yardas a convertir:");
                  yardas = lector. nextDouble();
                  metros = yardas * 0.91;
-                 System.out.println(yardas+"yardas equivalen a "+metros+"metros");
+                 System.out.println(yardas+"yardas equivalen a "+metros+"Metros");
                  break;
                  
 
              case 3:
                  System.out.println("Ingrese  la cantidad de kilometros a convertir:");
-                 millas = lector. nextDouble();
-                 kilometros = millas * 0.62;
-                 System.out.println(millas+"Kilometro equivalen a "+Millas+"Millas");
+                 kilometros = lector. nextDouble();
+                 millas = kilometros * 0.62;
+                 System.out.println(kilometros+"Kilometro equivalen a "+millas+"Millas");
                  break;
                  
+                 
+                 case 4: 
+                 System.out.println("Ingrese  la cantidad de kilogramos a convertir:");
+                 kilogramos = lector. nextDouble();
+                 libras = kilogramos * 2.2;
+                 System.out.println(kilogramos+"kilogramos equivalen a "+libras+"Libras");
+                 break;
              default:
                  System.out.println("La opcion seleccionada no es valida.");
                  
